4ef7b51d5848e0323c9da0bc0383df29fea35481
Aniket <anidandawate1@gmail.com>
2018-05-27 01:50:16 +0530

    Few updates

--- HighBarAppWF/Form1.cs
+++ HighBarAppWF/Form1.cs
@@ -29,9 +29,9 @@ namespace HighBarAppWF
             comboBox1.SelectedIndex = 0;
 
             //bind doctors dropdown to doctors list
-            comboBox1.DataSource = clinics;
-            comboBox1.DisplayMember = "Name";
-            comboBox1.ValueMember = "Id";
+            comboBox3.DataSource = doctors;
+            comboBox3.DisplayMember = "Name";
+            comboBox3.ValueMember = "Id";
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -51,12 +51,17 @@ namespace HighBarAppWF
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+            MessageBox.Show("Date - "+dateTimePicker1.Value.ToString("dd/MM/yyyy")+" Clinic selected: " + ((Clinic)this.comboBox1.SelectedItem).Code);
+
+            //uncomment code when in network of API.
+
             //get the fukcing doctors
-            var doctors = HttpService.GetDoctorsByClinicAsync(this.comboBox1.SelectedItem.ToString(), this.dateTimePicker1.Value.ToString("dd/MM/yyyy")).Result;
+            //var doctors = HttpService.GetDoctorsByClinicAsync(this.comboBox1.SelectedItem.ToString(), this.dateTimePicker1.Value.ToString("dd/MM/yyyy")).Result;
             //Add them to the dropdown 
-            doctors.ForEach(x => comboBox3.Items.Add(new { Text = x.Name, Value = x.Id }));
+            //doctors.ForEach(x => comboBox3.Items.Add(new { Text = x.Name, Value = x.Id }));
             //Make default selection
-            comboBox3.SelectedIndex = 0;
+            //comboBox3.SelectedIndex = 0;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -66,7 +71,7 @@ namespace HighBarAppWF
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Date - " + dateTimePicker1.Value.ToString("dd/MM/yyyy") + " Clinic selected: " + ((Clinic)this.comboBox1.SelectedItem).Code);
         }
     }
 }
